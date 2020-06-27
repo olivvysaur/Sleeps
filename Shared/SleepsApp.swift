7b85c5abce6439217c6f376a59458df12bb118ba
Olivia Asch <olivia.c.asch@gmail.com>
2020-06-27 12:14:34 +0100

    Initial Commit

---
+++ Shared/SleepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepsApp.swift
+//  Shared
+//
+//  Created by Olivia Asch on 27/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct SleepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
